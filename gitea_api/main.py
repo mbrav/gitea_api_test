9163da7fce77533fac44346b2a6f333057b952f9
mbrav <mbrav@protonmail.com>
2022-01-15 01:03:32 +0300

    cleanup

--- gitea_api/main.py
+++ gitea_api/main.py
@@ -1,10 +1,8 @@
 import json
-import pprint
 
 import urllib3
 
 http = urllib3.PoolManager()
-pp = pprint.PrettyPrinter(indent=1)
 
 BASE_URL = 'http://localhost:3000'
 
@@ -19,12 +17,18 @@ class GiteaAPI:
         self.password = password
         self.token = token
 
-    def get_headers(self):
+    @staticmethod
+    def health_check():
+        r = http.request('GET', BASE_URL)
+        assert r.status == 200
+
+    def _get_headers(self):
         """Генерируем хедары на основе наличия токена"""
 
         if self.token:
             headers = {
-                'Authorization': f'token {self.token}'
+                'Authorization': f'token {self.token}',
+                'Content-Type': 'application/json'
             }
             return headers
         headers = urllib3.make_headers(
@@ -34,11 +38,11 @@ class GiteaAPI:
     def create_user(self):
         """Создаём пользователя"""
 
-        data = {
+        data = json.dumps({
             'email': self.email,
             'password': self.password,
             'username': self.username,
-        }
+        })
         # user = NewUser(**data)
         r = http.request('POST', BASE_URL)
 
@@ -49,36 +53,36 @@ class GiteaAPI:
             'name': self.username,
         }
 
-        headers = self.get_headers()
+        headers = self._get_headers()
 
         url = f'{BASE_URL}/api/v1/users/{self.username}/tokens'
 
         r = http.request('POST', url, fields=data, headers=headers)
 
-        assert r.status == 201
-
         r_data = json.loads(r.data.decode('utf-8'))
         self.token = r_data.get('sha1')
-        print(f'Token {self.token}')
+
+        assert r.status == 201
+        return r.status, r.data
 
     def get_repositories(self):
         """Получаем список репозиторий"""
 
-        headers = self.get_headers()
+        headers = self._get_headers()
 
         url = f'{BASE_URL}/api/v1/user/repos'
         r = http.request('GET', url, headers=headers)
-        assert r.status == 200
 
         r_data = json.loads(r.data.decode('utf-8'))
 
-        pp.pprint(r_data)
+        assert r.status == 200
+        return r.status, r.data
 
     def create_repository(self, name: str = 'test_repo',
                           description: str = 'Test Repository'):
         """Создаём новый репозитории"""
 
-        data = {
+        data = json.dumps({
             'auto_init': True,
             'default_branch': 'master',
             'description': description,
@@ -88,23 +92,24 @@ class GiteaAPI:
             'private': False,
             'template': True,
             'trust_model': 'default'
-        }
+        })
 
-        headers = self.get_headers()
+        headers = self._get_headers()
 
         url = f'{BASE_URL}/api/v1/user/repos'
-        r = http.request('POST', url, fields=data, headers=headers)
-        assert r.status == 201
+        r = http.request('POST', url, body=data, headers=headers)
 
         r_data = json.loads(r.data.decode('utf-8'))
-        pp.pprint(r_data)
+
+        assert r.status == 201
+        return r.status, r.data
 
     def add_file_to_repository(self,
                                repo: str,
                                filepath: str):
         """Создаём новый фаил в репозитории"""
 
-        data = {
+        data = json.dumps({
             'author': {
                 'email': self.email,
                 'name': self.username
@@ -113,47 +118,49 @@ class GiteaAPI:
                 'email': self.email,
                 'name': self.username
             },
-            'content': b'ZGVmIHRlc3QoKToKICAgIHByaW50KCd0ZXN0JykK',
+            'content': 'ZGVmIHRlc3QoKToKICAgIHByaW50KCd0ZXN0JykK',
             'message': f'Add {filepath}',
-        }
+        })
 
-        headers = self.get_headers()
+        headers = self._get_headers()
 
         url = f'{BASE_URL}/api/v1/repos/{self.username}/{repo}/contents/{filepath}'
-        r = http.request('POST', url, fields=data, headers=headers)
-        # assert r.status == 201
+        r = http.request('POST', url, body=data, headers=headers)
 
         r_data = json.loads(r.data.decode('utf-8'))
-        pp.pprint(r_data)
+
+        assert r.status == 201
+        return r.status, r.data
 
     def get_file_from_repository(self,
                                  repo: str,
                                  filepath: str):
         """Получаем фаил из репозитории"""
 
-        headers = self.get_headers()
+        headers = self._get_headers()
 
         url = f'{BASE_URL}/api/v1/repos/{self.username}/{repo}/contents/{filepath}'
         r = http.request('GET', url, headers=headers)
-        assert r.status == 200
 
         r_data = json.loads(r.data.decode('utf-8'))
-        pp.pprint(r_data)
+
+        assert r.status == 200
+        return r.status, r.data
 
 
 if __name__ == '__main__':
-    r = http.request('GET', BASE_URL)
-    assert r.status == 200
+    GiteaAPI.health_check()
 
     data = {
         'username': 'test',
         'email': 'test@test.com',
         'password': 'test12',
-        'token': '9b329b9aaf76f6a469618a8a5ce9e3a577a33c0e',
+        # 'token': '3a66b345916f911756bae5da1158bbc2169c4422',
     }
 
     user = GiteaAPI(**data)
-    # user.get_repositories()
-    # user.create_repository(name='test_repo')
+    user.create_token()
+    user.get_repositories()
+    user.create_repository(name='test_repo')
     user.add_file_to_repository(repo='test_repo', filepath='main.py')
-    # user.get_file_from_repository(repo='test_repo', filepath='README.md')
+    user.get_file_from_repository(repo='test_repo', filepath='main.py')
